d01315dbc410c43d15d0642b7f9ed6066262f66d
rahul yadav <rahul.yadavv7000@gmail.com>
2024-05-16 17:46:20 +0530

    data transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -10,8 +10,9 @@ from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
-from src.components.model_trainer import ModelTrainerConfig
-from src.components.model_trainer import ModelTrainer
+#from src.components.model_trainer import ModelTrainerConfig
+#from src.components.model_trainer import ModelTrainer
+
 @dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts',"train.csv")
@@ -48,11 +49,16 @@ class DataIngestion:
             )
         except Exception as e:
             raise CustomException(e,sys)
+
+
         
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
 
-    
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
+
+   
 
 
--- src/utils.py
+++ src/utils.py
@@ -4,9 +4,9 @@ import sys
 import numpy as np 
 import pandas as pd
 import dill
-import pickle
-from sklearn.metrics import r2_score
-from sklearn.model_selection import GridSearchCV
+#import pickle
+#from sklearn.metrics import r2_score
+#from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
@@ -17,11 +17,12 @@ def save_object(file_path, obj):
         os.makedirs(dir_path, exist_ok=True)
 
         with open(file_path, "wb") as file_obj:
-            pickle.dump(obj, file_obj)
+            dill.dump(obj, file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
-    
+
+"""
 def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
         report = {}
@@ -60,3 +61,4 @@ def load_object(file_path):
 
     except Exception as e:
         raise CustomException(e, sys)
+"""
